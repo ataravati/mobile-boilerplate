87c81adc1afd662dcf814464c77fda5f7db44266
Shawn Axsom <shawn.axsom@prominentedge.com>
2018-10-19 16:37:43 -0400

    prettier cleanup

--- src/screens/todo-list/todo-list.test.tsx
+++ src/screens/todo-list/todo-list.test.tsx
@@ -12,7 +12,7 @@ import console = require("console");
 // https://github.com/react-navigation/react-navigation/issues/2269
 // React Navigation generates random React keys, which makes 
 // snapshot testing fail. Mock the randomness to keep from failing.
-jest.mock('react-navigation/src/routers/KeyGenerator', () => ({
+jest.mock("react-navigation/src/routers/KeyGenerator", () => ({
   generateKey: jest.fn(() => 123)
 }));
 
