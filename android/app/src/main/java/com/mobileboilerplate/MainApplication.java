359645e75f6220bdedfb4cf3db22531b1bbcecfe
Aliyar Taravati <ataravati@gmail.com>
2018-12-21 12:59:26 -0600

    Added and linked react-native-sound...

--- android/app/src/main/java/com/mobileboilerplate/MainApplication.java
+++ android/app/src/main/java/com/mobileboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobileboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new MainReactPackage());
+      return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new RNSoundPackage());
     }
 
     @Override
