924bd70fc38696e9f81d88a6f90ac26920bc1b05
Aliyar Taravati <ataravati@gmail.com>
2018-12-26 19:48:11 -0600

    Moved the seek-bar to a separate component...

--- src/screens/pointless-screen/audio-player.tsx
+++ src/screens/pointless-screen/audio-player.tsx
@@ -3,6 +3,7 @@ import { View, Text, Button } from "native-base";
 import { ActivityIndicator, Slider } from "react-native";
 import Sound from "react-native-sound";
 import { Platform } from "react-native";
+import SeekBar from "./seek-bar";
 
 Sound.setCategory("Playback", true);
 if (Platform.OS === "ios") {
@@ -70,69 +71,46 @@ export default class AudioPlayer extends React.Component<
   onSeek = time => {
     this.sound.setCurrentTime(time);
     this.setState({ currentTime: time });
-    this.play();
   };
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "flex-end" }}>
+      <View style={{ flex: 1, justifyContent: "center" }}>
         <ActivityIndicator
           size="large"
           color="#0000ff"
           animating={!this.state.isLoaded}
         />
-        <View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Button onPress={this.onPressPlay} disabled={!this.state.isLoaded}>
-              <Text>{this.state.isPlaying === false ? "Play" : "Pause"}</Text>
-            </Button>
+        {this.state.isLoaded && (
+          <View style={{ flex: 1, justifyContent: "flex-end" }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Button
+                onPress={this.onPressPlay}
+                disabled={!this.state.isLoaded}
+              >
+                <Text>{this.state.isPlaying === false ? "Play" : "Pause"}</Text>
+              </Button>
+            </View>
+            <SeekBar
+              duration={this.state.duration}
+              currentTime={this.state.currentTime}
+              onSeek={time => this.onSeek(time)}
+              onPlay={() => this.play()}
+              onPause={() => this.pause()}
+            />
           </View>
-          <View style={{ flexDirection: "row", direction: "ltr" }}>
-            <Text>Elapsed: {this.formatTime(this.state.currentTime)}</Text>
-            <Text style={{ marginStart: 20 }}>
-              Remaining:{" "}
-              {this.formatTime(this.state.duration - this.state.currentTime)}
-            </Text>
-          </View>
-          <Slider
-            style={{ direction: "ltr", marginTop: 10 }}
-            maximumValue={this.getSliderMaxValue()}
-            value={this.state.currentTime}
-            onTouchStart={() => this.pause()}
-            onSlidingComplete={value => this.onSeek(value)}
-          />
-        </View>
+        )}
       </View>
     );
   }
 
-  getSliderMaxValue() {
-    return Math.max(1, this.state.duration, this.state.currentTime);
-  }
-
-  formatTime(time) {
-    if (time < 1) return "00:00";
-
-    let result = "";
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = Math.floor(time % 60);
-
-    if (hours > 0) result += hours + ":";
-
-    result += this.padZero(minutes) + ":" + this.padZero(seconds);
-
-    return result;
-  }
-
-  padZero(number) {
-    if (number < 10) return "0" + number;
-    return number;
-  }
+  // componentWillUnmount() {
+  //   this.sound.release();
+  // }
 }
---
+++ src/screens/pointless-screen/seek-bar.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { View, Text } from "native-base";
+import { Slider } from "react-native";
+
+const formatTime = (time: number) => {
+  if (time < 1) return "00:00";
+
+  let result = "";
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = Math.floor(time % 60);
+
+  if (hours > 0) result += hours + ":";
+
+  result += padZero(minutes) + ":" + padZero(seconds);
+
+  return result;
+};
+
+const padZero = (number: number) => {
+  if (number < 10) return "0" + number;
+  return number;
+};
+
+const SeekBar = ({ duration, currentTime, onSeek, onPlay, onPause }) => {
+  const elapsed = formatTime(currentTime);
+  const remaining = formatTime(duration - currentTime);
+  return (
+    <View>
+      <View style={{ flexDirection: "row", direction: "ltr" }}>
+        <Text>Elapsed: {elapsed}</Text>
+        <Text style={{ marginStart: 20 }}>Remaining: {remaining}</Text>
+      </View>
+      <Slider
+        style={{ direction: "ltr", marginTop: 10 }}
+        maximumValue={Math.max(1, duration, currentTime)}
+        value={currentTime}
+        onResponderMove={() => onPause()}
+        onResponderRelease={() => onPlay()}
+        onSlidingComplete={value => onSeek(value)}
+      />
+    </View>
+  );
+};
+
+export default SeekBar;
