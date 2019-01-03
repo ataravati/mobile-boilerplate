991bfd7d1c481faa3911ad85cb49f15f389db62d
Aliyar Taravati <ataravati@gmail.com>
2019-01-03 12:43:02 -0600

    Added download functionality to the episodes screen...

--- src/screens/episodes/episodes.tsx
+++ src/screens/episodes/episodes.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Header } from "native-base";
 import { ScrollView, StyleSheet } from "react-native";
 import { Episode } from "./episode";
 import { NavigationScreenProp, NavigationState } from "react-navigation";
+import RNFetchBlob from "rn-fetch-blob";
 
 interface Episode {
   title: string;
@@ -45,8 +46,8 @@ export class Episodes extends React.Component<EpisodesProps, void> {
     },
     {
       title: "مستند صوتی «بازیِ گوش» قسمت نهم: با پیمان یزدانیان",
-      filename:
-        "http://feeds.soundcloud.com/stream/325004379-tehranpodcast-hermes09.mp3",
+      filename: "325004379-tehranpodcast-hermes09.mp3",
+      // "http://feeds.soundcloud.com/stream/325004379-tehranpodcast-hermes09.mp3",
     },
   ];
 
@@ -62,6 +63,19 @@ export class Episodes extends React.Component<EpisodesProps, void> {
 
   downloadEpisode = (episode: Episode) => {
     console.log(`Downloading episode ${episode.title}`);
+    const dirs = RNFetchBlob.fs.dirs;
+    const filename = episode.filename.split("/").pop();
+    RNFetchBlob.config({
+      fileCache: true,
+      path: dirs.MainBundleDir + "/" + filename,
+    })
+      .fetch("GET", episode.filename)
+      .progress((received: number, total: number) => {
+        console.log(`${received} of ${total} downloaded...`);
+      })
+      .then(res => {
+        console.log(`The file saved to ${res.path()}`);
+      });
   };
 
   render() {
