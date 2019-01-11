f240874fa73b11dc90ebbdf95c1cffb38c6e2856
Aliyar Taravati <ataravati@gmail.com>
2019-01-10 19:06:06 -0600

    Updated the unit tests...

--- src/stores/audio-player-store.test.ts
+++ src/stores/audio-player-store.test.ts
@@ -1,24 +1,45 @@
 import { audioPlayerStore } from "./audio-player-store";
+import { Episode } from "./episode-store";
 
 jest.mock("react-native-sound");
+jest.mock("rn-fetch-blob", () => "RNFetchBlob");
+
 describe("audio-player-store", () => {
   it("can load a local audio file", () => {
-    const path = "sample_audio.mp3";
-    return audioPlayerStore.load(path).then(() => {
+    const episode = Episode.create({
+      id: "1",
+      title: "test",
+      url: "https://sample-videos.com/audio/mp3/wave.mp3",
+      localPath: "sample_audio.mp3",
+      isLocal: true,
+      isDownloading: false,
+    });
+
+    return audioPlayerStore.load(episode).then(() => {
       expect(audioPlayerStore.isLoading).toEqual(false);
       expect(audioPlayerStore.duration).toEqual(500);
       expect(audioPlayerStore.paused).toEqual(true);
-      expect(audioPlayerStore.path).toEqual(path);
+      expect(audioPlayerStore.episode).not.toBeNull();
+      expect(audioPlayerStore.episode!.id).toEqual(episode.id);
     });
   });
 
   it("can load a remote audio file", () => {
-    const path = "https://sample-videos.com/audio/mp3/wave.mp3";
-    return audioPlayerStore.load(path).then(() => {
+    const episode = Episode.create({
+      id: "1",
+      title: "test",
+      url: "https://sample-videos.com/audio/mp3/wave.mp3",
+      localPath: null,
+      isLocal: false,
+      isDownloading: false,
+    });
+
+    return audioPlayerStore.load(episode).then(() => {
       expect(audioPlayerStore.isLoading).toEqual(false);
       expect(audioPlayerStore.duration).toEqual(500);
       expect(audioPlayerStore.paused).toEqual(true);
-      expect(audioPlayerStore.path).toEqual(path);
+      expect(audioPlayerStore.episode).not.toBeNull();
+      expect(audioPlayerStore.episode!.id).toEqual(episode.id);
     });
   });
 });
