a619d5aa4449bf4c02dcb16e3f22021666b088d8
Aliyar Taravati <ataravati@gmail.com>
2019-01-17 14:40:17 -0600

    Added and linked react-native-track-player...

--- android/app/src/main/java/com/mobileboilerplate/MainApplication.java
+++ android/app/src/main/java/com/mobileboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobileboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.guichaguri.trackplayer.TrackPlayer;
 import io.realm.react.RealmReactPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new TrackPlayer(),
             new RealmReactPackage(),
             new RNFetchBlobPackage(),
             new RNSoundPackage());
