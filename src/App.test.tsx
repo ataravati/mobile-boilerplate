524bc73f3c5dec1487205c0f61a12553a8787c10
Aliyar Taravati <ataravati@gmail.com>
2019-01-22 09:40:12 -0600

    Mocked react-native-track-player...

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,16 @@ import renderer from "react-test-renderer";
 import { App } from "./App";
 
 jest.mock("rn-fetch-blob", () => "RNFetchBlob");
+jest.mock("react-native-track-player", () => {
+  const TrackPlayerMocked = () => {};
+
+  const addEventListenerMocked = jest.fn((event, callback) => {});
+  TrackPlayerMocked.addEventListener = addEventListenerMocked.bind(
+    TrackPlayerMocked,
+  );
+
+  return TrackPlayerMocked;
+});
 
 describe("app", () => {
   it("can render snapshot", () => {
